48e65778e2b2bc7285ef43ac373e1a5e51ec85a4
Nathaniel Caldwell <nbcwell@gmail.com>
2011-12-09 19:54:11 -0500

    Added license information, and removed a couple of needless imports.

--- src/com/youkaicountry/blackkitten/BlackKitten.java
+++ src/com/youkaicountry/blackkitten/BlackKitten.java
@@ -1,3 +1,23 @@
+// Copyright (c) <2011> <Nathaniel Caldwell>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 package com.youkaicountry.blackkitten;
 
 //TODO: New time controls: Play, Stop, Pause, Reset, Update Init
@@ -10,10 +30,8 @@ package com.youkaicountry.blackkitten;
 //TODO: Track Bonds attached to a particle
 //TODO: Be able to get bond index by the 2 particles it connects
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Random;
 
 import com.youkaicountry.blackkitten.IntQueue;
